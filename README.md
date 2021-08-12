f92d5aa50d397c4665b5f09f414a604c3b561c6e
zenodep <zenodep@gmail.com>
2021-08-12 09:32:33 +0200

    Upated main.py with docstrings

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Hi
 
 # Hello
+
+# This is my first project with Git and Github.
--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from time import sleep as _sleep
 from keyboard import is_pressed as key_is_pressed
 
 def countdown(seconds):
+    """Counts down from given amount of seconds
+    
+    Args:
+        seconds (int): seconds to count down from
+    """    
     for i in range(seconds, -1, -1):
         if i == 0:
             print('Time is up!')
@@ -10,7 +15,13 @@ def countdown(seconds):
         _sleep(1)
 
 def stopwatch(key='q'):
+    """Counts up per second until key is pressed
+
+    Args:
+        key (str): Key to stop the timer. Defaults to 'q'.
+    """    
     i = 0
+    print(f'Stopwatch started. Press {key} to stop')
     while not key_is_pressed(key):
         _sleep(1)
         i += 1
